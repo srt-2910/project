ef0d7eeafc463d9eb4dde30ce2933ccf7186b070
srt-2910 <95272098+srt-2910@users.noreply.github.com>
2021-11-30 15:08:06 +0530

    Add files via upload

---
+++ weprovide.js
@@ -0,0 +1,20 @@
+$('.owl-carouse').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:4,
+            nav:false
+        },
+        1000:{
+            items:12,
+            nav:false,
+            loop:true,
+        }
+    }
+})
